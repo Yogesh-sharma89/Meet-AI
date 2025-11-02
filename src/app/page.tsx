ffe59e6754138cce8cb59d6c6e542211eb6af070
Yogesh Sharma <nitya.upmanyu@gmail.com>
2025-11-02 11:26:27 +0530

    01:Setup Project

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-full p-4 flex items-center gap-5'>
+      Hey I am Meet AI 
+      <Button>Meet Ai</Button>
+    </div>
+  )
+}
+
+export default Home
